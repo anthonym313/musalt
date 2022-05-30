6b0251a2487c050f6f6c7f1008c6dc451e1b61f0
Anthony <anthony.a.s.martin@gmail.com>
2022-05-30 16:09:13 -0400

    start form

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,6 +6,10 @@ import {Loader} from './'
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
+const Input = ({placeholder}) => (
+    <input placeholder={placeholder}/>
+)
+
 const Welcome = () => {
     const connectWallet = () => {
 
@@ -68,6 +72,9 @@ const Welcome = () => {
                             </div>
 
                         </div>
+                    </div>
+                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                        <Input placeholder="Address To" type="text" handleChange={()=>{}}/>
                     </div>
                 </div>
             </div>
