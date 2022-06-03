d0d12d08873b86ad0686ee8a861aa069d7e61d6f
Anthony <anthony.a.s.martin@gmail.com>
2022-06-03 16:49:22 -0400

    add transaction message

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 
 import dummyData from '../utils/dummyData';
-import {shortenAddress} from '../utils/shortenAddress';
+import { shortenAddress } from '../utils/shortenAddress.js';
 
 const TransactionCard =({addressTo, addressFrom, timestamp,message, keyword, amount, url})=>(
     <div className='bg-[181918] m-4 flex flex-1 
@@ -13,8 +13,20 @@ const TransactionCard =({addressTo, addressFrom, timestamp,message, keyword, amo
         flex-col p-3 rounded-md hover:shadow-2xl
     '>
         <div className='flex flex-col items-center w-full mt-3'>
-            <div className='flex justify-start w-full mb-6 p-2'>
-                <a href={`https://ropsten.etherscan.io/address/${addressFrom}`}></a>
+            <div className='w-full mb-6 p-2'>
+                <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener noreferrer">
+                    <p className='text-white text-base'> From: { shortenAddress(addressFrom)}</p>
+                </a>
+                <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener noreferrer">
+                    <p className='text-white text-base'> To: {shortenAddress(addressTo)}</p>
+                </a>
+                <p className='text-white text-base'>Amount: {amount} ETH</p>
+                {message && (
+                    <>
+                        <br/>
+                        <p className='text-white text-base'>Message: {message}</p>
+                    </>
+                )}
             </div>
         </div>
     </div>
