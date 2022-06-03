ae7a2f86007cb180dab62f3c68514f277104b515
Anthony <anthony.a.s.martin@gmail.com>
2022-06-03 16:36:10 -0400

    work on transaction card, connect to etherscan

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -2,6 +2,23 @@ import React, {useContext} from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 
 import dummyData from '../utils/dummyData';
+import {shortenAddress} from '../utils/shortenAddress';
+
+const TransactionCard =({addressTo, addressFrom, timestamp,message, keyword, amount, url})=>(
+    <div className='bg-[181918] m-4 flex flex-1 
+        2xl:min-w-[450px]
+        2xl:max-w-[500px]
+        sm:min-w-[2700px]
+        sm:max-w-[300px]
+        flex-col p-3 rounded-md hover:shadow-2xl
+    '>
+        <div className='flex flex-col items-center w-full mt-3'>
+            <div className='flex justify-start w-full mb-6 p-2'>
+                <a href={`https://ropsten.etherscan.io/address/${addressFrom}`}></a>
+            </div>
+        </div>
+    </div>
+)
 
 const Transactions = () => {
     const {currentAccount} = useContext(TransactionContext);
@@ -13,7 +30,11 @@ const Transactions = () => {
                 ):(
                     <h3 className='text-white text-3xl text-center my-2'>Connect your account to see the Latest transactions</h3>
                 )}
-                {/*Connect your account to see the Latest transactions */}
+                <div className='flex flex-wrap justify-center items-center mt-10'>
+                    {dummyData.reverse().map((transaction,i)=>(
+                        <TransactionCard key={i}{...transaction}/>
+                    ))}
+                </div>
             </div>
         </div>
     )
